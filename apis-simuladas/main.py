ce1959d57de1352ef2763ab26ec340b612196058
cassiobotaro <cassiobotaro@gmail.com>
2021-11-23 20:17:51 -0300

    Corrige problemas apresentados na api simulada

--- apis-simuladas/main.py
+++ apis-simuladas/main.py
@@ -5,29 +5,31 @@ from uuid import UUID
 from random import random
 
 
-from fastapi import FastAPI, HTTPException
-from fastapi import status
+from fastapi import FastAPI, HTTPException, Header, status
+from fastapi.param_functions import Depends
 from fastapi.responses import JSONResponse
+from fastapi.routing import APIRouter
 from starlette.responses import PlainTextResponse
 
 DATA_DIR = Path(os.path.dirname(__file__)) / "data"
 FAIL_RATE = int(os.getenv("FAIL_RATE", 0))
 
 
-app = FastAPI()
+async def verify_x_tenant_id(x_tenant_id: str = Header(...)):
+    if x_tenant_id != "21fea73c-e244-497a-8540-be0d3c583596":
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail={"message": "Tenant ID not on tenants list"})
+    return x_tenant_id
 
-@app.middleware("http")
-async def validate_x_tenant_id(request, call_next):
-    if request.headers.get("X-Tenant-ID") != "21fea73c-e244-497a-8540-be0d3c583596":
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content={"message": "Tenant ID not on tenants list"})
-    return await call_next(request)
+async def verify_apikey(x_api_key: str = Header(...)):
+    if x_api_key != "5734143a-595d-405d-9c97-6c198537108f":
+        raise HTTPException(status.HTTP_403_FORBIDDEN, detail="Unauthorized")
+    return x_api_key
 
 
-@app.middleware("http")
-async def validate_apikey(request, call_next):
-    if request.headers.get("X-API-KEY") != "5734143a-595d-405d-9c97-6c198537108f":
-        return PlainTextResponse(status_code=status.HTTP_403_FORBIDDEN, content="Unauthorized")
-    return await call_next(request)
+app = FastAPI(dependencies=[Depends(verify_apikey)])
+maestro = APIRouter(dependencies=[Depends(verify_x_tenant_id)])
+account = APIRouter()
+catalogs = APIRouter()
 
 
 @app.middleware("http")
@@ -41,13 +43,13 @@ def read_data(file_):
     return json.load(open(file_))
 
 
-@app.get("/catalogs")
+@catalogs.get("/catalogs", tags=["catalogs"])
 async def catalog_list():
     files = (DATA_DIR / "catalogo").glob("*.json")
     return [read_data(f_) for f_ in files]
 
 
-@app.get("/catalogs/{code}")
+@catalogs.get("/catalogs/{code}", tags=["catalogs"])
 async def catalog_retrieve(code):
     try:
         return read_data(DATA_DIR / "catalogo" /  f"{code}.json")
@@ -55,17 +57,17 @@ async def catalog_retrieve(code):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
 
 
-@app.get("/account/v1/whoami", tags=["account"])
+@account.get("/account/v1/whoami", tags=["account"])
 def whoami():
     return read_data(DATA_DIR / "account" / "whoami.json")
 
 
-@app.get("/account/v1/whoami/tenants", tags=["account"])
+@account.get("/account/v1/whoami/tenants", tags=["account"])
 def whoami_tenants():
     return read_data(DATA_DIR / "account" / "whoami_tenants.json")
 
 
-@app.get("/maestro/v1/orders", tags=["maestro"])
+@maestro.get("/maestro/v1/orders", tags=["maestro"])
 def orders(_limit: int = 10, _offset: int = 0):
     if _limit != 10:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Limit must be 10")
@@ -77,7 +79,7 @@ def orders(_limit: int = 10, _offset: int = 0):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=read_data(DATA_DIR / "maestro" / "orders" / "not_found.json"))
 
 
-@app.get("/maestro/v1/orders/{order_id}", tags=["maestro"])
+@maestro.get("/maestro/v1/orders/{order_id}", tags=["maestro"])
 def order(order_id: UUID):
     try:
         return read_data(DATA_DIR / "maestro" / "order" / f"{order_id}.json")
@@ -87,7 +89,7 @@ def order(order_id: UUID):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=error)
 
 
-@app.get("/maestro/v1/orders/{order_id}/packages/{package_id}", tags=["maestro"])
+@maestro.get("/maestro/v1/orders/{order_id}/packages/{package_id}", tags=["maestro"])
 def packages(order_id: UUID, package_id: UUID):
     try:
         return read_data(DATA_DIR / "maestro" / "packages" / f"{order_id}{package_id}.json")
@@ -95,9 +97,13 @@ def packages(order_id: UUID, package_id: UUID):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=read_data(DATA_DIR / "maestro" / "packages" / "not_found.json"))
 
 
-@app.get("/maestro/v1/orders/{order_id}/packages/{package_id}/items", tags=["maestro"])
+@maestro.get("/maestro/v1/orders/{order_id}/packages/{package_id}/items", tags=["maestro"])
 def package_items(order_id: UUID, package_id: UUID):
     try:
         return read_data(DATA_DIR / "maestro" / "package_items" / f"{order_id}{package_id}.json")
     except FileNotFoundError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=read_data(DATA_DIR / "maestro" / "package_items" / "not_found.json"))
+
+app.include_router(maestro)
+app.include_router(account)
+app.include_router(catalogs)
